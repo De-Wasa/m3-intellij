1acc7e8b73d9718078c5ab73e24f83eec3cde45f
davidwasa <david.wasa@marfeel.com>
2018-09-30 20:16:55 +0200

    Move money logic away

--- src/main/java/com/wasa/mcube/IntellijWorkshop/service/CoinUtils.java
+++ src/main/java/com/wasa/mcube/IntellijWorkshop/service/CoinUtils.java
@@ -1,8 +1,13 @@
 package com.wasa.mcube.IntellijWorkshop.service;
 
 import com.wasa.mcube.IntellijWorkshop.entity.Coin;
+import org.apache.commons.lang3.mutable.MutableDouble;
 
+import java.util.EnumSet;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class CoinUtils {
 
@@ -10,5 +15,24 @@ public class CoinUtils {
         return money.stream()
                 .mapToDouble(Coin::getValue)
                 .sum();
+    }
+
+    static List<Coin> calculateChange(double total, double cost) {
+        MutableDouble moneyLeft = new MutableDouble(total - cost);
+        return EnumSet.allOf(Coin.class).stream()
+                .sorted(CoinUtils::moreValueFirst)
+                .flatMap(coin -> IntStream
+                        .range(0, floor(moneyLeft, coin))
+                        .mapToObj(value -> coin))
+                .peek(coin -> moneyLeft.subtract(coin.getValue()))
+                .collect(toList());
+    }
+
+    private static int floor(MutableDouble moneyLeft, Coin coin) {
+        return (int) Math.floor(moneyLeft.getValue() / coin.getValue());
+    }
+
+    private static int moreValueFirst(Coin o1, Coin o2) {
+        return Double.compare(o2.getValue(), o1.getValue());
     }
 }
--- src/main/java/com/wasa/mcube/IntellijWorkshop/service/VendingMachineImpl.java
+++ src/main/java/com/wasa/mcube/IntellijWorkshop/service/VendingMachineImpl.java
@@ -3,17 +3,13 @@ package com.wasa.mcube.IntellijWorkshop.service;
 import com.wasa.mcube.IntellijWorkshop.entity.Coin;
 import com.wasa.mcube.IntellijWorkshop.entity.Item;
 import com.wasa.mcube.IntellijWorkshop.exception.NotEnoughMoneyException;
-import org.apache.commons.lang3.mutable.MutableDouble;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static com.wasa.mcube.IntellijWorkshop.service.CoinUtils.sum;
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
 
 @Component
 public class VendingMachineImpl implements VendingMachine {
@@ -34,14 +30,7 @@ public class VendingMachineImpl implements VendingMachine {
             throw new NotEnoughMoneyException();
         }
 
-        MutableDouble moneyLeft = new MutableDouble(total - cost);
-        money = EnumSet.allOf(Coin.class).stream()
-                .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
-                .flatMap(coin -> IntStream
-                        .range(0, (int) Math.floor(moneyLeft.getValue() / coin.getValue()))
-                        .mapToObj(value -> coin))
-                .peek(coin -> moneyLeft.subtract(coin.getValue()))
-                .collect(toList());
+        money = CoinUtils.calculateChange(total, cost);
 
         return item;
     }
